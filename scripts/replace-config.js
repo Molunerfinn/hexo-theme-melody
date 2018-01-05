d7d8f4d8133ecfd263864d2e1d5f1160810ca15a
Molunerfinn <marksz@teamsz.xyz>
2018-01-05 10:58:28 +0800

    Fixed: version control

--- scripts/replace-config.js
+++ scripts/replace-config.js
@@ -1,6 +1,13 @@
 /**
  * Note: configs in _data/melody.yml will replace configs in hexo.theme.config.
  */
+
+const version = require('../package.json').version
+
+hexo.extend.helper.register('version', function () {
+  return version
+})
+
 hexo.on('generateBefore', function () {
   if (hexo.locals.get) {
     var data = hexo.locals.get('data')
