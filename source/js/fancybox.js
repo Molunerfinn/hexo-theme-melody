8a241f43015daefed81e4095371bea43435df27e
Molunerfinn <marksz@teamsz.xyz>
2018-03-11 23:20:44 +0800

    Added: gallery layout

--- source/js/fancybox.js
+++ source/js/fancybox.js
@@ -4,7 +4,13 @@ $(function () {
     imgList = $('#post-content img')
   }
   for (var i = 0; i < imgList.length; i++) {
-    var $a = $('<a href="' + imgList[i].src + '" data-fancybox="group" data-caption="' + imgList[i].alt + '" class="fancybox"></a>')
+    var $a = $(
+      '<a href="' +
+        imgList[i].src +
+        '" data-fancybox="group" data-caption="' +
+        imgList[i].alt +
+        '" class="fancybox"></a>'
+    )
     var alt = imgList[i].alt
     var $wrap = $(imgList[i]).wrap($a)
     if (alt) {
@@ -16,5 +22,27 @@ $(function () {
     selector: '[data-fancybox]',
     loop: true,
     transitionEffect: 'slide'
+  })
+
+  var galleryItem = $('.gallery-item')
+  var galleryList = []
+  galleryItem.each(function (idx, elem) {
+    galleryList.push({
+      src: $(elem).data('url'),
+      opts: {
+        caption: $(elem).data('title')
+      }
+    })
+  })
+  galleryItem.on('click', function () {
+    $.fancybox.open(
+      galleryList,
+      {
+        loop: true,
+        transitionEffect: 'slide'
+      },
+      galleryItem.index(this)
+    )
+    return false
   })
 })
