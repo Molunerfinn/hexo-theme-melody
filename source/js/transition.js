52f430bd26e2e682d12594524a19674fba319ccb
PiEgg <marksz@teamsz.xyz>
2019-10-09 16:23:11 +0800

    :sparkles: Feature: add sidebar_display option for controlling the sidebar display
    ISSUES CLOSED: #203

--- source/js/transition.js
+++ source/js/transition.js
@@ -1,19 +1,21 @@
 $(function () {
   // page
-  $('.layout').velocity('stop')
+  $('.layout')
+    .velocity('stop')
     .velocity('transition.slideUpIn', {
       delay: 500,
       duration: 1000,
       easing: 'easeInOutQuart',
       complete: function () {
-        if ($('.sidebar-toc').length > 0) {
+        if ($('#sidebar').data('display')) {
           setTimeout(function () {
             $('#toggle-sidebar').click()
           }, 200)
         }
       }
     })
-  $('#top-container').velocity('stop')
+  $('#top-container')
+    .velocity('stop')
     .velocity('transition.fadeIn', {
       delay: 500,
       duration: 1000,
